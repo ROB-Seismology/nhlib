12b806b3f9a0678838fe8c8e3adfa1d6b9c826dc
root <root@seishaz.oma.be>
2013-03-14 10:06:23 +0000

    Updated compile_speedups to openquake namespace

--- speedups/compile_speedups.sh
+++ speedups/compile_speedups.sh
@@ -1,2 +1,4 @@
-gcc -I/usr/include/python2.7 geoutilsmodule.c -lpython2.7 -shared -fPIC -O3 -o ../nhlib/geo/_utils_speedups.so
-gcc -I/usr/include/python2.7 geodeticmodule.c -lpython2.7 -shared -fPIC -O3 -o ../nhlib/geo/_geodetic_speedups.so
+#! /bin/bash
+
+gcc -I/usr/include/python2.7 geoutilsmodule.c -lpython2.7 -shared -fPIC -O3 -o ../openquake/hazardlib/geo/_utils_speedups.so
+gcc -I/usr/include/python2.7 geodeticmodule.c -lpython2.7 -shared -fPIC -O3 -o ../openquake/hazardlib/geo/_geodetic_speedups.so
