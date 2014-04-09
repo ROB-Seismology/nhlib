194e8417bedff340d7f31a01c1632ba1bca818d6
Kris Vanneste <kris.vanneste@oma.be>
2014-04-09 10:57:00 +0200

    Added IPE's BakunWentworth1997 and Barrientos2007

---
+++ openquake/hazardlib/gsim/bakun_wentworth_1997.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+# The Hazard Library
+# Copyright (C) 2012 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Module exports :class:`BakunWentworth1997`.
+"""
+from __future__ import division
+
+import numpy as np
+
+from openquake.hazardlib.imt import MMI
+
+
+class BakunWentworth1997(IPE):
+    """
+    Implements IPE developed by Bakun & Wentworth (1997)
+    California
+    Epicentral distance range 0 - 150 km
+    """
+
+    DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.ACTIVE_SHALLOW_CRUST
+
+    DEFINED_FOR_INTENSITY_MEASURE_TYPES = set([
+        MMI
+    ])
+
+    DEFINED_FOR_STANDARD_DEVIATION_TYPES = set([
+        const.StdDev.TOTAL
+    ])
+
+    # TODO !
+    REQUIRES_SITES_PARAMETERS = set(('vs30', ))
+
+    REQUIRES_RUPTURE_PARAMETERS = set(('mag'))
+
+    REQUIRES_DISTANCES = set(('repi', ))
+
+
+    def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
+        """
+        See :meth:`superclass method
+        <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
+        for spec of input and result values.
+        """
+
+        # Eq. 6, page 1508
+        mean_mmi = 5.07 + 10.9 * rup.mag - 3.69 * np.log10(dists.repi)
+
+        stddevs = np.zeros_like(dists.repi)
+
+        return mean_mmi, stddevs
+
+    def compute_site_term(self, sites):
+        # TODO !
+        pass
---
+++ openquake/hazardlib/gsim/barrientos_2007.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+# The Hazard Library
+# Copyright (C) 2012 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Module exports :class:`Barrientos2007`.
+"""
+from __future__ import division
+
+import numpy as np
+
+from openquake.hazardlib.imt import MMI
+
+
+class Barrientos2007(IPE):
+    """
+    Implements IPE developed by Barrientos (2007)
+    Chile
+    MS!
+    """
+
+    DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.SUBDUCTION_INTERFACE
+
+    DEFINED_FOR_INTENSITY_MEASURE_TYPES = set([
+        MMI
+    ])
+
+    DEFINED_FOR_STANDARD_DEVIATION_TYPES = set([
+        const.StdDev.TOTAL
+    ])
+
+    # TODO !
+    REQUIRES_SITES_PARAMETERS = set(('vs30', ))
+
+    REQUIRES_RUPTURE_PARAMETERS = set(('mag'))
+
+    REQUIRES_DISTANCES = set(('rrup', ))
+
+
+    def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
+        """
+        See :meth:`superclass method
+        <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
+        for spec of input and result values.
+        """
+
+        mean_mmi = (1.3844 * rup.mag - 3.755 * np.log10(dists.rrup)
+                    - 0.0006 * dists.rrup + 3.91)
+        mean_mmi += self.compute_site_term(sites)
+
+        stddevs = np.zeros_like(dists.rrup)
+
+        return mean_mmi, stddevs
+
+    def compute_site_term(self, sites):
+        # TODO !
+        return 0
