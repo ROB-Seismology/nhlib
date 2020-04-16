131a31aacc8c7fc15e6b574e5f274c69211f7fce
Kris Vanneste <kris.vanneste@oma.be>
2020-04-16 14:49:04 +0200

    Renamed calc_ln_CAV and calc_CAV_exceedance_prob functions to calc_ln_cav and calc_cav_exceedance_prob, respectively.

--- openquake/hazardlib/gsim/cav.py
+++ openquake/hazardlib/gsim/cav.py
@@ -3,6 +3,8 @@ This module implements CAV (cumulative absolute velocity) filtering,
 as outlined in Abrahamson et al. (2006) (EPRI technical paper 1014099)
 """
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import numpy as np
 
 
@@ -14,7 +16,7 @@ ln_0_2 = np.log(0.2)
 ln_0_025 = np.log(0.025)
 
 
-def calc_ln_CAV(ln_pga, mag, ln_vs30):
+def calc_ln_cav(ln_pga, mag, ln_vs30):
 	"""
 	Calculate natural logarithm of CAV using a two-step approach including
 	duration dependence, based on an empirical model of CAV in the U.S.
@@ -51,8 +53,13 @@ def calc_ln_CAV(ln_pga, mag, ln_vs30):
 	indexes_below = np.where(ln_pga <= ln_1)
 	indexes_above = np.where(ln_pga > ln_1)
 
-	ln_CAV[indexes_below] = C0 + C1 * (mag - 6.5) + C2 * (mag - 6.5)**2 + C3 * ln_pga + C4 * ln_pga**2 + C5 * ln_pga**3 + C6 * ln_pga**4 + C7 * (ln_vs30 - 6) + C8 * ln_dur_uni + C9 * ln_dur_uni**2
-	ln_CAV[indexes_above] = C0 + C1 * (mag - 6.5) + C2 * (mag - 6.5)**2 + C3 * ln_pga + C7 * (ln_vs30 - 6) + C8 * ln_dur_uni + C9 * ln_dur_uni**2
+	ln_CAV[indexes_below] = (C0 + C1 * (mag - 6.5) + C2 * (mag - 6.5)**2
+							+ C3 * ln_pga + C4 * ln_pga**2 + C5 * ln_pga**3
+							+ C6 * ln_pga**4 + C7 * (ln_vs30 - 6)
+							+ C8 * ln_dur_uni + C9 * ln_dur_uni**2)
+	ln_CAV[indexes_above] = (C0 + C1 * (mag - 6.5) + C2 * (mag - 6.5)**2
+							+ C3 * ln_pga + C7 * (ln_vs30 - 6)
+							+ C8 * ln_dur_uni + C9 * ln_dur_uni**2)
 
 	## Calculate standard deviation
 	sigma_ln_CAV1 = np.zeros_like(ln_dur_uni)
@@ -61,17 +68,19 @@ def calc_ln_CAV(ln_pga, mag, ln_vs30):
 	sigma_ln_CAV1[idxs] = 0.37 - 0.09 * (ln_dur_uni[idxs] - ln_0_2)
 	sigma_ln_CAV1[ln_dur_uni < ln_0_2] = 0.37
 
-	sigma_ln_CAV = np.sqrt((C8 + 2 * C9 * ln_dur_uni)**2 * sigma_ln_dur_uni**2 + sigma_ln_CAV1**2)
+	sigma_ln_CAV = np.sqrt((C8 + 2 * C9 * ln_dur_uni)**2 * sigma_ln_dur_uni**2
+							+ sigma_ln_CAV1**2)
 
 	return (ln_CAV, sigma_ln_CAV)
 
 
 def calc_ln_dur_uni(ln_pga, mag, ln_vs30):
 	"""
-	Calculate the natural logarithm of the uniform duration (in s) above 0.025 g,
-	based on an empirical model for the U.S.
-	The uniform duration is the total time during which the absolute value of the
-	acceleration time series exceeds a specified threshold (0.025 g in the case of CAV)
+	Calculate the natural logarithm of the uniform duration (in s)
+	above 0.025 g, based on an empirical model for the U.S.
+	The uniform duration is the total time during which the absolute
+	value of the acceleration time series exceeds a specified threshold
+	(0.025 g in the case of CAV)
 
 	:param ln_pga:
 		1-D numpy array, natural log of peak horizontal acceleration in g
@@ -96,12 +105,13 @@ def calc_ln_dur_uni(ln_pga, mag, ln_vs30):
 	a7 = -0.355 #(0.020)
 	sigma_ln_dur_uni = 0.509
 
-	ln_dur_uni = a1 + a2 * ln_pga + a3 / (ln_pga + a4) + a5 * (mag - 6.5) + a6 * (mag - 6.5)**2 + a7 * (ln_vs30 - 6)
+	ln_dur_uni = (a1 + a2 * ln_pga + a3 / (ln_pga + a4) + a5 * (mag - 6.5)
+				+ a6 * (mag - 6.5)**2 + a7 * (ln_vs30 - 6))
 
 	return (ln_dur_uni, sigma_ln_dur_uni)
 
 
-def calc_CAV_exceedance_prob(ln_pga, mag, vs30, cav_min=0.16):
+def calc_cav_exceedance_prob(ln_pga, mag, vs30, cav_min=0.16):
 	"""
 	Calculate probability of exceeding a specified CAV value
 
@@ -134,9 +144,10 @@ def calc_CAV_exceedance_prob(ln_pga, mag, vs30, cav_min=0.16):
 		prob = np.zeros_like(ln_pga, 'd')
 		ln_vs30 = np.log(vs30)
 
-		ln_CAV, sigma_ln_CAV = calc_ln_CAV(ln_pga, mag, ln_vs30)
+		ln_CAV, sigma_ln_CAV = calc_ln_cav(ln_pga, mag, ln_vs30)
 
-		epsilon_CAV = (np.log(cav_min) - ln_CAV[non_zero_indexes]) / sigma_ln_CAV[non_zero_indexes]
+		epsilon_CAV = ((np.log(cav_min) - ln_CAV[non_zero_indexes])
+						/ sigma_ln_CAV[non_zero_indexes])
 		prob[non_zero_indexes] = 1.0 - norm.cdf(epsilon_CAV)
 	else:
 		prob = np.ones_like(ln_pga, 'd')
